617057b4e8898f3fa1946639a44fd6aff51750de
Yeison Betancourt S <yeisonbe10@hotmail.com>
2025-01-06 17:20:13 -0500

    Setup @tanstack/react-query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = async () => {
-    try {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["provider"],
+    queryFn: async () => {
       const apiUrl = import.meta.env.VITE_API_URL;
       const url = `${apiUrl}/api/providers?address=123 Main St, Anytown, USA`;
       const response = await fetch(url);
       const data = await response.json();
-      console.log("🚀 ~ fetchData ~ response:", data);
-    } catch (error) {
-      console.log("error", error);
-    }
-  };
+      return data;
+    },
+  });
+
+  useEffect(() => {
+    console.log("data", data);
+  }, [data]);
+
+  if (isPending) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
 
   return (
     <>
@@ -34,9 +36,6 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
